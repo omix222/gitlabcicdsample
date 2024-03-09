76a6eed1964abe80a63c217134504252b130e5a3
Hiromi Takahashi <hiromix222@gmail.com>
2024-03-09 10:14:45 +0900

    first commit

---
+++ src/main/java/com/example/demo/RestController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
